c79d86fb5475620145fba3ef205dde7bdb450e31
Channaka Gunawardhane <channakagunawardana@gmail.com>
2023-12-10 17:08:32 +0530

    creating-bloc

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,5 @@
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
-const String newsAPIKey = 'ff957763c54c44d8b00e5e082bc76cb0';
+const String newsAPIKey = 'aa1b1027cafa466690609bd07fddc590';
 const String countryQuery = 'us';
 const String categoryQuery = 'general';
 const String kDefaultImage =
--- lib/features/daily_news/data/data_sources/remote/news_api_service.dart
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -1,1 +1,19 @@
-abstract class NewsApiService{}
+import 'package:dio/dio.dart';
+import 'package:flutter_newsapp_clean/core/constants/constants.dart';
+import 'package:flutter_newsapp_clean/features/daily_news/data/models/article.dart';
+
+import 'package:retrofit/retrofit.dart';
+
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET('/top-headlines')
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query('apiKey') String? apiKey,
+    @Query('country') String? country,
+    @Query('category') String? category,
+  });
+}
